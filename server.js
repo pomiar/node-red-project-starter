92480b4069374dc6ad5e855fb66201bc43fb2043
Dave Conway-Jones <conway@uk.ibm.com>
2018-03-23 13:41:20 +0000

    best yet

--- server.js
+++ server.js
@@ -9,11 +9,5 @@ var command;
 
 if (args.find(a => a === '-f')) { const npm_install = exec('npm install -f'); }
 
-if (port) { command = spawn('node', ['node_modules/node-red/red.js', '-v', '-p', port, '-userDir', '.']); }
-else { command = spawn('node', ['node_modules/node-red/red.js', '-v', '-userDir', '.']); }
-
-command.stdout.on('data', (data) => { console.log(data.toString()); });
-
-command.stderr.on('data', (data) => { console.log(data.toString()); });
-
-command.on('close', (code) => { console.log(`child process exited with code ${code}`); });
+if (port) { command = spawn('node', ['node_modules/node-red/red.js', '-p', port, '-userDir', '.'], {stdio:'inherit'}); }
+else { command = spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], {stdio:'inherit'}); }
