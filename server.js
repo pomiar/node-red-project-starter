38958f5a8ea24488a26ba49758a0edd7c4639059
Dave Conway-Jones <conway@uk.ibm.com>
2019-05-29 16:40:16 +0100

    Auto open web page (if local)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const spawn = require('child_process').spawn;
 const exec = require('child_process').execSync;
+const open = require('open');
 
 var args = process.argv;
 var port = parseInt(args[2]);
@@ -10,3 +11,5 @@ if (args.find(a => a === '-f')) { const npm_install = exec('npm install -f'); }
 
 if (port) { spawn('node', ['node_modules/node-red/red.js', '-p', port, '-userDir', '.'], {stdio:'inherit'}); }
 else { spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], {stdio:'inherit'}); }
+
+(async () => { await open('http://localhost:'+(port || 1880)); })();
