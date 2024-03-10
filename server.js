e1780f4b8f3cf07aeabccb7202f12bb908ddf792
pomiar <98620823+pomiar@users.noreply.github.com>
2024-03-10 22:36:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,18 @@
 
 const spawn = require('child_process').spawn;
 const exec = require('child_process').execSync;
-const open = require('open');
+// const open = require('open');
 
 var args = process.argv;
 var port = parseInt(args[2]);
 
 if (args.find(a => a === '-f')) { const npm_install = exec('npm install -f'); }
 
-if (port) { spawn('node', ['node_modules/node-red/red.js', '-p', port, '-userDir', '.'], {stdio:'inherit'}); }
-else { spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], {stdio:'inherit'}); }
+if (port) { spawn('node', ['node_modules/node-red/red.js', '-p', port, '-userDir', '.'], { stdio: 'inherit' }); }
+else { spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], { stdio: 'inherit' }); }
 
-(async () => { await open('http://localhost:'+(port || 1880)); })();
+// (async () => { await open('http://localhost:'+(port || 1880)); })();
+(async () => {
+    const openModule = await import('open');
+    await openModule.default('http://localhost:' + (port || 1880));
+})();
